6b38ec9efbb3f7480da9aead607a2874b9964f1e
Yash_10 <yashyt01@gmail.com>
2026-06-19 19:45:33 +0000

    Initial

--- src/RiskEngine.sol
+++ src/RiskEngine.sol
@@ -1,4 +1,5 @@
 //SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24
 contract RiskEngine {
     function getRiskScore(uint256 tradeSize, uint256 volatility) external pure returns (uint256) {
         uint256 sizeFactor = (tradeSize * 100) / 150 ether;
