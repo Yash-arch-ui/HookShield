0ec0d229b8cdec86c837a507d25f94b700ae4dcd
Yash_10 <yashyt01@gmail.com>
2026-07-17 15:23:30 +0000

    CORRECTED WORKLFOW

--- script/DeployPoolManager.s.sol
+++ script/DeployPoolManager.s.sol
@@ -6,10 +6,12 @@ import {PoolManager} from "../lib/v4-core/src/PoolManager.sol";
 
 contract DeployPoolManagerScript is Script {
     function run() external {
+
         uint256 pk = vm.envUint("PRIVATE_KEY");
+        address deployer = vm.addr(pk);
         vm.startBroadcast(pk);
-        PoolManager manager = new PoolManager(msg.sender);
-        console.log(address(manager));
+        PoolManager manager = new PoolManager(deployer);
+         console.log("PoolManager deployed at:", address(manager));
         vm.stopBroadcast();
     }
 }
