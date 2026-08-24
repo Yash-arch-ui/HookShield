955d63106cee5f3528a553a94731ba23755bf526
Yash_10 <yashyt01@gmail.com>
2026-08-24 19:32:14 +0000

    Fmt

--- test/unit/WhaleScoreSignal.t.sol
+++ test/unit/WhaleScoreSignal.t.sol
@@ -17,35 +17,37 @@ contract WhaleScoreSignalTest is Test {
     SignalState signalState;
     WhaleScoreSignal whaleSignal;
     PoolId poolId;
+
     function setUp() public {
         poolManager = new PoolManager(address(this));
         signalState = new SignalState();
         whaleSignal = new WhaleScoreSignal(address(poolManager), address(signalState));
         signalState.setAuthorizedWriter(address(whaleSignal), true);
-        PoolKey memory key = PoolKey ({
+        PoolKey memory key = PoolKey({
             currency0: Currency.wrap(address(0x1)),
             currency1: Currency.wrap(address(0x2)),
             fee: 3000,
             tickSpacing: 60,
-            hooks:  IHooks(address(0))
+            hooks: IHooks(address(0))
         });
         poolManager.initialize(key, TickMath.getSqrtPriceAtTick(0));
         poolId = key.toId();
-
     }
+
     function test_Compute_ZeroLiquidity_ReturnsMaxScore() public view {
         // no liquidity rn ; should be at max risk
         uint256 score = whaleSignal.compute(poolId, 1e17, true);
         assertEq(score, 1e18);
     }
-    function testComputeZeroAmountReturnsZero() public view{
-        uint256 score = whaleSignal.compute(poolId, 0 , true);
+
+    function testComputeZeroAmountReturnsZero() public view {
+        uint256 score = whaleSignal.compute(poolId, 0, true);
         assertEq(score, 0);
     }
+
     function testUpdatePublishesToSignalState() public {
-          whaleSignal.update(poolId, 1e17, true);
+        whaleSignal.update(poolId, 1e17, true);
         SignalSnapshot memory snap = signalState.getSnapshot(poolId);
         assertEq(snap.whaleScore, 1e18);
-
     }
 }
